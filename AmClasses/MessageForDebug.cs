ad67d41d42ee32b4cc4c2b34efbabaceef2a5130
LarPN <larpn@bratsk-city.ru>
2015-10-28 10:25:25 +0800

    добавил класс

---
+++ AmClasses/MessageForDebug.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AMClasses
+{
+    public class MessageForDebug
+    {
+        public string Body;
+        public string Debug;
+        public string Step;
+        public string Exception;
+    }
+}
